699b7305b4198ec300d1c67642f973075034f910
KangChou <kangchou666@gmail.com>
2022-03-31 10:19:54 +0800

    Update run.sh

--- c_so_cpp2022/cpp_test_so/run.sh
+++ c_so_cpp2022/cpp_test_so/run.sh
@@ -7,3 +7,12 @@ g++ -c func.cpp
 g++ -c main.cpp
 g++ main.o func.o -o test2
 ./test2
+
+# 参考：https://www.cnblogs.com/mjk961/p/7862170.html
+# apt-get update --fix-missing
+# apt install gdb gcc g++
+#  g++ --version
+# g++ (Debian 10.2.1-6) 10.2.1 20210110
+# Copyright (C) 2020 Free Software Foundation, Inc.
+# This is free software; see the source for copying conditions.  There is NO
+# warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
