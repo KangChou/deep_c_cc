40ad51cf1ca766340c558a0c9d4195d3e5112438
KangChou <kangchou666@gmail.com>
2022-04-24 11:48:46 +0800

    Add files via upload

--- bazel_cc_dynamic_library/run.sh
+++ bazel_cc_dynamic_library/run.sh
@@ -1,12 +1,15 @@
 bazel build //cc_a_library:static
 
-bazel build //cc_so_library:dllfunc-0.0.1.so
+bazel build //cc_so_library:libfunc.so
 
 cd cc_a_library
 mv libstatic.a ../call_so_a_library
+cp static.h ../call_so_a_library
 
 cd ../
 cd cc_so_library
-mv libstatic.so ../call_so_a_library
+mv libfunc.so ../call_so_a_library
+cp dllfunc.h ../call_so_a_library
+
 cd ../
 bazel build //call_so_a_library:dll_test
